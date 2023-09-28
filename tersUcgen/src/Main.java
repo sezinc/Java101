d46e956cfad1ba8694ff9c233a00c5211748c18b
Sezin Civaroğlu <sezinkutluk@hotmail.com>
2023-09-28 09:29:16 +0300

    Add files via upload

---
+++ tersUcgen/src/Main.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+        int i = n-1;
+      /* while ( i < n)
+        {
+            for (int j = 0; j < (n - i); j++) {
+               // System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+               // System.out.print("*");
+            }
+            //System.out.println(" ");
+            i++;
+        }*/
+
+        while ( i >=0)
+        {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+            i--;
+        }
+
+    }
+}
